50dd9d77f6c7726952df73cd74606e384a1d62f7
Magda Ang <angmagda@gmail.com>
2014-05-29 14:44:29 -0500

    group project pseudocode

---
+++ week_3/6_group_project/gc1_pseudocode.js
@@ -0,0 +1,26 @@
+// My role in the group is: Translate user stories  into pseudocode
+ 
+
+// Here is my part of the challenge:
+
+//pseudocode
+{
+	
+	// Define function for sum of elements;
+	// Set parameters;
+		// Iterate with 'for' all the numbers in an array to get the total sum;
+	// Return sum value;
+
+	// Define function to get the mean of elements;
+	// Set parameters;
+		// Call the sum function and divide the total by the number of elements in the array;
+	// Return mean value;
+
+	// Define function to get the median of an array;
+	// Set parameters;
+		// Sort array;
+		// Define another var (middle or half) to store the value of sorted array / 2;
+		// Set this var equal to the array and index into it applying the Math.floor method to the array lenght -1 divided by two;
+		// Return half value;
+
+}
